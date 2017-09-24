e37c1ca6d3344152b30d5f3533840d1d9f15db64
Leonardo Cserny <leonardo_cserny@yahoo.com>
2017-09-24 12:07:34 +0300

    adds button loading in another thread

--- ui/src/main/java/net/cserny/videosmover/component/CustomTextFieldCell.java
+++ ui/src/main/java/net/cserny/videosmover/component/CustomTextFieldCell.java
@@ -1,5 +1,6 @@
 package net.cserny.videosmover.component;
 
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.Cursor;
@@ -12,16 +13,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import net.cserny.videosmover.model.SimpleVideoOutput;
-import net.cserny.videosmover.model.VideoMetadata;
-import net.cserny.videosmover.model.VideoRow;
+import net.cserny.videosmover.model.*;
 import net.cserny.videosmover.service.VideoMetadataService;
 import net.cserny.videosmover.service.helper.SimpleVideoOutputHelper;
 import org.controlsfx.control.PopOver;
 import org.controlsfx.control.textfield.CustomTextField;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 // TODO: refactor this and make prettier
@@ -72,68 +70,63 @@ public class CustomTextFieldCell extends TableCell<VideoRow, String> {
         button.setStyle("-fx-text-fill: WHITE; -fx-background-color: #01d277; -fx-cursor: hand;");
         button.setTooltip(new Tooltip("Some tooltip text"));
         button.setOnAction(event -> {
-            // TODO: make loading work
             ((ImageView) button.getGraphic()).setImage(loadingImage);
 
-            List<VideoMetadata> videoMetadataList = Collections.synchronizedList(new ArrayList<>());
             Runnable expensiveTask = () -> {
-//                VideoQuery videoQuery = VideoQuery.newInstance().withName(videoOutput.getName()).withYear(videoOutput.getYear()).build();
-//                if (videoOutput.getVideoType() == VideoType.MOVIE) {
-//                    videoMetadataList = metadataService.searchMovieMetadata(videoQuery);
-//                } else if (videoOutput.getVideoType() == VideoType.TVSHOW) {
-//                    videoMetadataList = metadataService.searchTvShowMetadata(videoQuery);
-//                }
-                ((ImageView) button.getGraphic()).setImage(altImage);
-            };
-            Thread loadingThread = new Thread(expensiveTask);
-            loadingThread.start();
-//            try {
-//                loadingThread.join();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-
-            PopOver popOver = new PopOver();
-            VBox vboxParent = new VBox();
-            for (VideoMetadata videoMetadata : videoMetadataList) {
-                HBox hbox = new HBox();
-                hbox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-                hbox.setOnMouseClicked(hboxEvent -> {
-                    customTextField.setText(SimpleVideoOutputHelper.formatOutput(videoOutput, videoMetadata));
-                    videoMetadata.setSelected(true);
-                    popOver.hide();
-                });
-                hbox.setCursor(Cursor.HAND);
-
-                hbox.getChildren().add(new ImageView(new Image(videoMetadata.getPosterUrl())));
-
-                VBox vbox = new VBox();
-                String metadataDescription = videoMetadata.getDescription();
-                if (metadataDescription.length() > 450) {
-                    metadataDescription = metadataDescription.substring(0, 450) + "...";
+                List<VideoMetadata> videoMetadataList = new ArrayList<>();
+                VideoQuery videoQuery = VideoQuery.newInstance().withName(videoOutput.getName()).withYear(videoOutput.getYear()).build();
+                if (videoOutput.getVideoType() == VideoType.MOVIE) {
+                    videoMetadataList = metadataService.searchMovieMetadata(videoQuery);
+                } else if (videoOutput.getVideoType() == VideoType.TVSHOW) {
+                    videoMetadataList = metadataService.searchTvShowMetadata(videoQuery);
                 }
-                Text title = new Text(videoMetadata.getName());
-                title.setStyle("-fx-font-weight: bold; -fx-fill: #081c24");
-                Text description = new Text(metadataDescription);
-                description.setWrappingWidth(500);
-                Text cast = new Text(String.join(", ", videoMetadata.getCast()));
-                cast.setWrappingWidth(500);
-                cast.setStyle("-fx-fill: #01d277");
-                vbox.getChildren().addAll(title, description, cast);
-                hbox.getChildren().add(vbox);
+                ((ImageView) button.getGraphic()).setImage(altImage);
 
-                vboxParent.getChildren().add(hbox);
-            }
-            popOver.setContentNode(vboxParent);
-            popOver.setArrowLocation(PopOver.ArrowLocation.RIGHT_CENTER);
-            popOver.setOnHidden(popEvent -> ((ImageView) button.getGraphic()).setImage(mainImage));
-            popOver.setOnShowing(popEvent -> ((ImageView) button.getGraphic()).setImage(altImage));
+                PopOver popOver = new PopOver();
+                VBox vboxParent = new VBox();
+                for (VideoMetadata videoMetadata : videoMetadataList) {
+                    HBox hbox = new HBox();
+                    hbox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+                    hbox.setOnMouseClicked(hboxEvent -> {
+                        customTextField.setText(SimpleVideoOutputHelper.formatOutput(videoOutput, videoMetadata));
+                        videoMetadata.setSelected(true);
+                        popOver.hide();
+                    });
+                    hbox.setCursor(Cursor.HAND);
 
-            if (popOver.isShowing()) {
-                popOver.hide();
-            } else {
-                popOver.show(button);
-            }
+                    hbox.getChildren().add(new ImageView(new Image(videoMetadata.getPosterUrl())));
+
+                    VBox vbox = new VBox();
+                    String metadataDescription = videoMetadata.getDescription();
+                    if (metadataDescription.length() > 450) {
+                        metadataDescription = metadataDescription.substring(0, 450) + "...";
+                    }
+                    Text title = new Text(videoMetadata.getName());
+                    title.setStyle("-fx-font-weight: bold; -fx-fill: #081c24");
+                    Text description = new Text(metadataDescription);
+                    description.setWrappingWidth(500);
+                    Text cast = new Text(String.join(", ", videoMetadata.getCast()));
+                    cast.setWrappingWidth(500);
+                    cast.setStyle("-fx-fill: #01d277");
+                    vbox.getChildren().addAll(title, description, cast);
+                    hbox.getChildren().add(vbox);
+
+                    vboxParent.getChildren().add(hbox);
+                }
+                popOver.setContentNode(vboxParent);
+                popOver.setArrowLocation(PopOver.ArrowLocation.RIGHT_CENTER);
+                popOver.setOnHidden(popEvent -> ((ImageView) button.getGraphic()).setImage(mainImage));
+                popOver.setOnShowing(popEvent -> ((ImageView) button.getGraphic()).setImage(altImage));
+
+                Platform.runLater(() -> {
+                    if (popOver.isShowing()) {
+                        popOver.hide();
+                    } else {
+                        popOver.show(button);
+                    }
+                });
+            };
+            new Thread(expensiveTask).start();
         });
 
         return button;
