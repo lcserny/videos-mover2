25868a1e66866c85101839d794eb345170aca99d
Leonardo Cserny <leonardo_cserny@netex.ro>
2017-11-06 14:46:19 +0200

    ISSUE-7: rename VideoCleanerSpec

--- core/src/test/java/net/cserny/videosmover/service/VideoCleanerSpec.java
+++ core/src/test/java/net/cserny/videosmover/service/VideoCleanerSpec.java
@@ -1,11 +1,10 @@
-package net.cserny.videosmover;
+package net.cserny.videosmover.service;
 
+import net.cserny.videosmover.ApplicationConfig;
 import net.cserny.videosmover.helper.InMemoryVideoFileSystemInitializer;
 import net.cserny.videosmover.helper.TestHelperConfig;
 import net.cserny.videosmover.helper.VideoCreationHelper;
 import net.cserny.videosmover.model.Video;
-import net.cserny.videosmover.service.VideoCleaner;
-import net.cserny.videosmover.service.VideoMover;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +19,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {ApplicationConfig.class, TestHelperConfig.class})
-public class TestVideoCleanup extends InMemoryVideoFileSystemInitializer {
+public class VideoCleanerSpec extends InMemoryVideoFileSystemInitializer {
     @Autowired
     private VideoCreationHelper videoHelper;
     @Autowired
@@ -38,12 +37,6 @@ public class TestVideoCleanup extends InMemoryVideoFileSystemInitializer {
     public void whenSourceParentFolderIsDownloadsThenDoNotRemoveIt() throws Exception {
         Video video = videoHelper.createMovie(DOWNLOADS_ROOT_VIDEO);
         assertCleaning(video, false);
-    }
-
-    @Test
-    public void afterMovingVideoSourceFolderShouldBeClean() throws Exception {
-        Video video = videoHelper.createMovie(DOWNLOADS_MOVIE_WITH_SUBTITLE);
-        assertCleaning(video, true);
     }
 
     @Test
