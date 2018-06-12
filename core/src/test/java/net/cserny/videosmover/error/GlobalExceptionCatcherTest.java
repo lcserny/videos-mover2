1773dc53f9395225be4a186e0133fa4ac253f4ee
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-06-12 10:35:24 +0300

    adds starter tests

--- core/src/test/java/net/cserny/videosmover/error/GlobalExceptionCatcherTest.java
+++ core/src/test/java/net/cserny/videosmover/error/GlobalExceptionCatcherTest.java
@@ -1,39 +1,43 @@
-//package net.cserny.videosmover.error;
-//
-//import net.cserny.videosmover.ApplicationConfig;
-//import net.cserny.videosmover.service.SimpleMessageRegistry;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.hamcrest.Matchers.containsString;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = {ApplicationConfig.class})
-//public class GlobalExceptionCatcherTest {
-//    private static String DISPLAYED_MESSAGE;
-//
-//    @Autowired
-//    private GlobalExceptionCatcher globalExceptionCatcher;
-//    @Autowired
-//    private SimpleMessageRegistry messageRegistry;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        messageRegistry.registerDisplayProvider(message -> { DISPLAYED_MESSAGE = message.getContent(); });
-//        Thread.setDefaultUncaughtExceptionHandler(globalExceptionCatcher);
-//    }
-//
-//    @Test
-//    public void uncaughtException_whenExceptionIsThrownAnywhere() throws Exception {
-//        String exceptionMessage = "Some exception";
-//        Thread testThread = new Thread(() -> { throw new RuntimeException(exceptionMessage); });
-//        testThread.start();
-//        testThread.join();
-//        Assert.assertThat(DISPLAYED_MESSAGE, containsString(exceptionMessage));
-//    }
-//}
+package net.cserny.videosmover.error;
+
+import net.cserny.videosmover.DaggerTestCoreComponent;
+import net.cserny.videosmover.TestCoreComponent;
+
+import net.cserny.videosmover.service.SimpleMessageRegistry;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+public class GlobalExceptionCatcherTest {
+
+    private static String DISPLAYED_MESSAGE;
+
+    @Inject
+    GlobalExceptionCatcher globalExceptionCatcher;
+
+    @Inject
+    SimpleMessageRegistry messageRegistry;
+
+    @Before
+    public void setUp() {
+        TestCoreComponent component = DaggerTestCoreComponent.create();
+        component.inject(this);
+
+        messageRegistry.registerDisplayProvider(message -> { DISPLAYED_MESSAGE = message.getContent(); });
+        Thread.setDefaultUncaughtExceptionHandler(globalExceptionCatcher);
+    }
+
+    @Test
+    public void uncaughtException_whenExceptionIsThrownAnywhere() throws Exception {
+        String exceptionMessage = "Some exception";
+        Thread testThread = new Thread(() -> { throw new RuntimeException(exceptionMessage); });
+        testThread.start();
+        testThread.join();
+        Assert.assertThat(DISPLAYED_MESSAGE, containsString(exceptionMessage));
+    }
+}
--- core/src/test/java/net/cserny/videosmover/service/OutputResolverTest.java
+++ core/src/test/java/net/cserny/videosmover/service/OutputResolverTest.java
@@ -1,48 +1,56 @@
-//package net.cserny.videosmover.service;
-//
-//import net.cserny.videosmover.ApplicationConfig;
-//import net.cserny.videosmover.helper.InMemoryVideoFileSystemInitializer;
-//import net.cserny.videosmover.helper.VideoCreator;
-//import net.cserny.videosmover.model.Video;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.containsString;
-//import static org.junit.Assert.assertTrue;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ApplicationConfig.class)
-//public class OutputResolverTest extends InMemoryVideoFileSystemInitializer {
-//    @Autowired
-//    private OutputResolver outputResolver;
-//
-//    @Test
-//    public void resolve_allDigitsMovieResolvesNameCorrectly() throws Exception {
-//        Video video = VideoCreator.createMovie(DOWNLOADS_MOVIE_ALL_DIGITS, outputResolver);
-//        assertThat(video.getOutput().getFileName().toString(), containsString("1922 (2017)"));
-//    }
-//
-//    @Test
-//    public void resolve_tvShowInputReturnsCorrectOutput() throws Exception {
-//        Video video = VideoCreator.createTvShow(DOWNLOADS_TVSHOW, outputResolver);
-//        assertTrue(video.getOutput().startsWith(StaticPathsProvider.getTvShowsPath()));
-//        assertTrue(video.getOutput().getFileName().toString().equals("Game Of Thrones"));
-//    }
-//
-//    @Test
-//    public void resolve_movieInputReturnsCorrectOutput() throws Exception {
-//        Video video = VideoCreator.createMovie(DOWNLOADS_MOVIE_WITH_SUBTITLE, outputResolver);
-//        assertTrue(video.getOutput().startsWith(StaticPathsProvider.getMoviesPath()));
-//        assertTrue(video.getOutput().getFileName().toString().equals("The Big Sick (2017)"));
-//    }
-//
-//    @Test
-//    public void resolve_tvShowInputSetsOutputToExistingTvShowName() throws Exception {
-//        Video video = VideoCreator.createTvShow(DOWNLOADS_EXISTING_TVSHOW, outputResolver);
-//        assertTrue(video.getOutput().getFileName().toString().equals("Criminal Minds"));
-//    }
-//}
+package net.cserny.videosmover.service;
+
+import net.cserny.videosmover.DaggerTestCoreComponent;
+import net.cserny.videosmover.TestCoreComponent;
+import net.cserny.videosmover.helper.InMemoryVideoFileSystemInitializer;
+import net.cserny.videosmover.helper.StaticPathsProvider;
+import net.cserny.videosmover.helper.VideoCreator;
+import net.cserny.videosmover.model.Video;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.*;
+
+public class OutputResolverTest extends InMemoryVideoFileSystemInitializer {
+
+    @Inject
+    OutputResolver outputResolver;
+
+    @Before
+    public void setUp() throws IOException {
+        super.setUp();
+        TestCoreComponent component = DaggerTestCoreComponent.create();
+        component.inject(this);
+    }
+
+    @Test
+    public void resolve_allDigitsMovieResolvesNameCorrectly() throws Exception {
+        Video video = VideoCreator.createMovie(DOWNLOADS_MOVIE_ALL_DIGITS, outputResolver);
+        assertThat(video.getOutputFilename(), containsString("1922 (2017)"));
+    }
+
+    @Test
+    public void resolve_tvShowInputReturnsCorrectOutput() throws Exception {
+        Video video = VideoCreator.createTvShow(DOWNLOADS_TVSHOW, outputResolver);
+        assertTrue(video.getOutputPath().startsWith(StaticPathsProvider.getTvShowsPath()));
+        assertEquals("Game Of Thrones", video.getOutputFilename());
+    }
+
+    @Test
+    public void resolve_movieInputReturnsCorrectOutput() throws Exception {
+        Video video = VideoCreator.createMovie(DOWNLOADS_MOVIE_WITH_SUBTITLE, outputResolver);
+        assertTrue(video.getOutputPath().startsWith(StaticPathsProvider.getMoviesPath()));
+        assertEquals("The Big Sick (2017)", video.getOutputFilename());
+    }
+
+    @Test
+    public void resolve_tvShowInputSetsOutputToExistingTvShowName() throws Exception {
+        Video video = VideoCreator.createTvShow(DOWNLOADS_EXISTING_TVSHOW, outputResolver);
+        assertEquals("Criminal Minds", video.getOutputFilename());
+    }
+}
