4a46850feee00e5c857e1876868c89915eedb83b
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-06-14 09:56:51 +0300

    adds logging

--- core/src/main/java/net/cserny/videosmover/error/GlobalExceptionCatcher.java
+++ core/src/main/java/net/cserny/videosmover/error/GlobalExceptionCatcher.java
@@ -3,12 +3,16 @@ package net.cserny.videosmover.error;
 import javafx.scene.control.Alert;
 import net.cserny.videosmover.model.Message;
 import net.cserny.videosmover.service.SimpleMessageRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class GlobalExceptionCatcher implements Thread.UncaughtExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionCatcher.class);
 
     private final SimpleMessageRegistry messageRegistry;
 
@@ -21,7 +25,10 @@ public class GlobalExceptionCatcher implements Thread.UncaughtExceptionHandler {
     public void uncaughtException(Thread t, Throwable e) {
         String content = e.toString();
         String title = e.getMessage() != null ? e.getMessage() : "Exception thrown";
-        messageRegistry.add(new Message(Alert.AlertType.ERROR, content, title));
+        Message message = new Message(Alert.AlertType.ERROR, content, title);
+        messageRegistry.add(message);
         messageRegistry.displayMessages();
+
+        LOGGER.error(message.getTitle(), e);
     }
 }
