4f22eeeb3c434fd8006c29ab381a2ea97fba0426
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-02-01 08:49:07 +0200

    issues/69: minor refactor

--- web/src/main/java/net/cserny/videosmover/web/ResourceExchangeProcessor.java
+++ web/src/main/java/net/cserny/videosmover/web/ResourceExchangeProcessor.java
@@ -33,6 +33,6 @@ public class ResourceExchangeProcessor implements HttpExchangeProcessor {
 
         OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(response);
-        outputStream.close();
+        outputStream.flush();
     }
 }
